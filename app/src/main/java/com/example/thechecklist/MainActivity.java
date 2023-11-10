003e89ab5aded4078c391d73d33ba841b94c7adc
SamSap1 <150430881+SamSap1@users.noreply.github.com>
2023-11-10 11:04:30 +0100

    hi

---
+++ app/src/main/java/com/example/thechecklist/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.thechecklist;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.thechecklist.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the button by its ID
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+
+        // Set a click listener for the button
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click, you can replace this with your actual logic
+                Toast.makeText(MainActivity.this, "Let's get started!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
